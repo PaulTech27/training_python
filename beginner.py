ccb9d7aebe5d243e89042a553802a7045e99c378
PaulTech27 <158353502+PaulTech27@users.noreply.github.com>
2026-02-26 19:42:09 +0000

    Create beginner.py
    add a simple python commands

---
+++ beginner.py
@@ -0,0 +1,40 @@
+print(123)
+print("Hello, World!")
+
+first_number=4
+second_number=4
+
+# this is my code
+if first_number > second_number:
+ print("first_number is greater than second_number!") 
+elif first_number == second_number:
+ print("equal")
+else:
+  print("first_number is less than second_number!")
+
+
+x = 'firstname'
+y = 'lastname'
+print(x+ ' ' + y)
+
+
+
+
+mynumber = 16
+for i in range(mynumber):
+ print('Number is --> ' + str(i))
+
+x = ["apple", "banana", "cherry"]
+
+for item in x:
+ print('I like to eat ' + item)
+ 
+print(bool(5>2))
+print(bool(5<2))
+
+
+def function(a , b):
+ print(a*b)
+
+
+function(a=3, b=4)
